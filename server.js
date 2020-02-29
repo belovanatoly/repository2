908578389ddaf53def50735018886095f1215bac
belovanatoly <61622458+belovanatoly@users.noreply.github.com>
2020-02-29 17:47:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
+const PORT = process.env.PORT || 5000
+
 var static = require('node-static');
 var http = require('http');
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
 	file.serve(req, res);
-}).listen(2017);
+}).listen(PORT);
 
-var io = require('socket.io').listen(app);
 console.log('begin');
-console.log('listening on port: 2017');
+console.log('Listening');
 
-
-io.sockets.on('connection', function (socket){
-	console.log('user connected');
-	socket.emit('success');
-
-});
 
