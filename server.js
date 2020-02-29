cd3e348baf600eb227ee3dc999bf41c6bef783e5
belovanatoly <61622458+belovanatoly@users.noreply.github.com>
2020-02-29 19:55:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 
 var static = require('node-static');
 var http = require('http');
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
 	file.serve(req, res);
-}).listen(PORT, () => console.log(`Listening on ${PORT}`));
+}).listen(PORT);
 
 console.log('begin');
-//console.log('Listening on ${PORT}');
+console.log('Listening on ' + PORT);
 
 var io = require('socket.io').listen(app);
 console.log('io is added');
